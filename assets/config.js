00603bc23a74d8305c466499afbaa6bca18b8f0a
SEBASTIEN KOENIG <sebastien.koenig@orange.fr>
2023-12-28 20:40:05 +0100

    creation de la bases du slide

---
+++ assets/config.js
@@ -0,0 +1,10 @@
+let SlidesZoneImg = document.querySelector(".banner-img")
+console.log(SlidesZoneImg)
+
+let clicZoneLeft = document.querySelector(".clic-left")
+console.log(clicZoneLeft)
+
+let clicZoneRight = document.querySelector(".clic-right")
+console.log(clicZoneRight)
+
+
