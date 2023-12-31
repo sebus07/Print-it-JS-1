4c2fcb5a32b3e33063b854f482bcecc810e22a4f
SEBASTIEN KOENIG <sebastien.koenig@orange.fr>
2023-12-31 17:38:25 +0100

    creation du slide js

--- assets/config.js
+++ assets/config.js
@@ -1,10 +1,37 @@
-let SlidesZoneImg = document.querySelector(".banner-img")
-console.log(SlidesZoneImg)
+let SlidesZoneImg = document.querySelector(".banner-img");
+console.log(SlidesZoneImg);
 
-let clicZoneLeft = document.querySelector(".clic-left")
-console.log(clicZoneLeft)
+let tagLine = document.querySelector("#banner p");
+console.log(tagLine);
 
-let clicZoneRight = document.querySelector(".clic-right")
-console.log(clicZoneRight)
+let currentIndex = 0;
 
+let clicZoneRight = document.querySelector(".clic-right");
+console.log(clicZoneRight);
+
+let clicZoneLeft = document.querySelector(".clic-left");
+console.log(clicZoneLeft);
+
+let dotsContainer = document.querySelector(".dots-container");
+console.log(dotsContainer);
+
+clicZoneRight.addEventListener("click", () => {
+    // Mettre à jour l'index de la diapositive
+    currentIndex = (currentIndex + 1) % slides.length;
+
+    // Mettre à jour l'image et le texte et le dots par la fonction
+    SlidesZoneImg.setAttribute("src", `./assets/images/slideshow/${slides[currentIndex].image}`);
+    tagLine.innerHTML = slides[currentIndex].tagLine;
+    updateDots();
+});
+
+clicZoneLeft.addEventListener("click", () => {
+    // Mettre à jour l'index de la diapositive avec prise en compte des valeurs négatives
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+
+    // Mettre à jour l'image et le texte et le dots par la fonction
+    SlidesZoneImg.setAttribute("src", `./assets/images/slideshow/${slides[currentIndex].image}`);
+    tagLine.innerHTML = slides[currentIndex].tagLine;
+    updateDots();
+});
 
--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,31 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+    {
+        "image": "slide1.jpg",
+        "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+    },
+    {
+        "image": "slide2.jpg",
+        "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+    },
+    {
+        "image": "slide3.jpg",
+        "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+    },
+    {
+        "image": "slide4.png",
+        "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+    }
+];
+
+function updateDots() {// Appelez cette fonction après chaque clic pour mettre à jour les points
+
+    let dots = dotsContainer.querySelectorAll('.dot');
+    
+    dots.forEach((dot, index) => {
+        if (index === currentIndex) {
+            dot.classList.add('active');
+        } else {
+            dot.classList.remove('active');
+        }
+    });
+}
